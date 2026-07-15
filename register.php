d0358cb9015028d11a68221ec5f59e9420f9b460
cfrancey <caleb.francey@education.nsw.gov.au>
2026-07-15 12:32:32 +1000

    Create register.php

---
+++ register.php
@@ -0,0 +1,5 @@
+<?php
+$host     = 'localhost'; 
+$db_name  = 'user_accounts'; // Replace with your actual database name
+$username = 'root';          // Default XAMPP username is 'root'
+$password = '';              // Default XAMPP password is empty ''
