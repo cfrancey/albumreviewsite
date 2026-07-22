fe4f3a54c70d54ae5d21b5442d2ccd3f30d23a90
cfrancey <caleb.francey@education.nsw.gov.au>
2026-07-22 11:53:03 +1000

    Delete register.php

--- register.php
+++
@@ -1,5 +0,0 @@
-<?php
-$host     = 'localhost'; 
-$db_name  = 'user_accounts'; // Replace with your actual database name
-$username = 'root';          // Default XAMPP username is 'root'
-$password = '';              // Default XAMPP password is empty ''
